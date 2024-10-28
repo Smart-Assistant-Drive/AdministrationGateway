a29e6124380493e392e44b8137471b81a51e6f8c
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2024-10-28 21:29:05 +0100

    chore: removed unused files

--- rest/src/main/kotlin/com/example/rest/businessLayer/adapter/login/LoginDataSourceResponseModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.rest.businessLayer.adapter.login
-
-import com.example.rest.domainLayer.Role
-
-data class LoginDataSourceResponseModel(
-    val name: String,
-    val password: String,
-    val role: Role,
-)
--- rest/src/main/kotlin/com/example/rest/businessLayer/adapter/token/TokenDataSourceResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.rest.businessLayer.adapter.token
-
-import com.example.rest.domainLayer.Role
-
-data class TokenDataSourceResponse(
-    val user: String,
-    val role: Role,
-)
