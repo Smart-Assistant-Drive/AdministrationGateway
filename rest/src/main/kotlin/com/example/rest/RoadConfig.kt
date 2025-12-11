11cc4cfee1d91530177ea5eaee2d727d799acbc2
DiLilloDaniele <daniele.dilillo@studio.unibo.it>
2025-12-11 15:35:03 +0100

    chore: added service properties

--- rest/src/main/kotlin/com/example/rest/RoadConfig.kt
+++ rest/src/main/kotlin/com/example/rest/RoadConfig.kt
@@ -12,7 +12,9 @@ class RoadConfig {
 	@Bean
 	fun roadInputBoundary(environment: Environment): RoadInputBoundary {
 		val remote = environment.getProperty("road.service.host") ?: "http://localhost:8070"
-		val signsDataSourceGateway = RoadRemoteDataSource(remote, "http://localhost:8070", "http://localhost:8091")
+        val remoteTrafficDt = environment.getProperty("road.service.trafficdt.host") ?: "http://localhost:8091"
+        val remoteSemaphoreDt = environment.getProperty("road.service.semaphoredt.host") ?: "http://localhost:8090"
+		val signsDataSourceGateway = RoadRemoteDataSource(remote, remoteSemaphoreDt, remoteTrafficDt)
 		return RoadUseCase(signsDataSourceGateway)
 	}
 }
